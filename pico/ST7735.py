ce26859443bbaa1c0897b300108d0f020ac83b9f
Benjamin Russell <benfrussell@gmail.com>
2024-07-14 20:36:43 -0400

    Update Pico script

---
+++ pico/ST7735.py
@@ -0,0 +1,430 @@
+from machine import Pin, SPI
+import time
+import framebuf
+from array import array
+from math import ceil, log, floor
+
+ST7735_NOP          = const(0x00)
+ST7735_SWRESET      = const(0x01)
+ST7735_RDDID        = const(0x04)
+ST7735_RDDST        = const(0x09)
+ST7735_SLPIN        = const(0x10)
+ST7735_SLPOUT       = const(0x11)
+ST7735_PTLON        = const(0x12)
+ST7735_NORON        = const(0x13)
+ST7735_INVOFF       = const(0x20)
+ST7735_INVON        = const(0x21)
+ST7735_DISPOFF      = const(0x28)
+ST7735_DISPON       = const(0x29)
+ST7735_CASET        = const(0x2A)
+ST7735_RASET        = const(0x2B)
+ST7735_RAMWR        = const(0x2C)
+ST7735_RAMRD        = const(0x2E)
+ST7735_PTLAR        = const(0x30)
+ST7735_VSCRDEF      = const(0x33)
+ST7735_COLMOD       = const(0x3A)
+ST7735_MADCTL       = const(0x36)
+ST7735_VSCRSADD     = const(0x37)
+ST7735_FRMCTR1      = const(0xB1)
+ST7735_FRMCTR2      = const(0xB2)
+ST7735_FRMCTR3      = const(0xB3)
+ST7735_INVCTR       = const(0xB4)
+ST7735_DISSET5      = const(0xB6)
+ST7735_PWCTR1       = const(0xC0)
+ST7735_PWCTR2       = const(0xC1)
+ST7735_PWCTR3       = const(0xC2)
+ST7735_PWCTR4       = const(0xC3)
+ST7735_PWCTR5       = const(0xC4)
+ST7735_VMCTR1       = const(0xC5)
+ST7735_RDID1        = const(0xDA)
+ST7735_RDID2        = const(0xDB)
+ST7735_RDID3        = const(0xDC)
+ST7735_RDID4        = const(0xDD)
+ST7735_PWCTR6       = const(0xFC)
+ST7735_GMCTRP1      = const(0xE0)
+ST7735_GMCTRN1      = const(0xE1)
+
+init_cmds = [
+    # SLPOUT - Sleep out & booster on
+    [ST7735_SLPOUT],
+    # Frame rate commands
+    # Frame rate = 333kHz / ( (param1 + 20) * (160 Lines + param2 + param3) )      
+    # Params: RTNA set 1-line period (0x02), FPA: front porch (0x2D), BPA: back porch (0x2E)
+    # Range: RTNA 0-7; FPA 0-63; BPA 1-63
+    # FRMCTR1 - Frame rate control (In normal mode/Full colors)  
+    [ST7735_FRMCTR1, 0x01, 0x2D],     
+    # FRMCTR2 - Frame rate control (In Idle mode/8-colors)                          
+    [ST7735_FRMCTR2, 0x01, 0x2D],
+    # FRMCTR3 - Frame rate control (In Partial mode/full colors) 
+    # Params 1-3 Line inversion mode; Params 4-6 Frame inversion mode
+    [ST7735_FRMCTR3, 0x01, 0x2C, 0x2D, 0x01, 0x2C, 0x2D],
+    # INVCTR - Display Inversion Control
+    # Params: 0 - Line Inversion, 1 - Frame Inversion
+    # 0b0000000X - Full Colors Normal Mode
+    # 0b000000X0 - Idle Mode
+    # 0b00000X00 - Full Colors Partial Mode
+    [ST7735_INVCTR, 0x07],
+    # DISSET5 - Signal & display configuration
+    [ST7735_DISSET5, 0xA2, 0x02, 0x84],
+    # PWCTR1 - GVDD voltage & AVDD uA
+    [ST7735_PWCTR1, 0xA2, 0x02, 0x84],
+    # PWCTR2 - Supply power level for VGH & VGL
+    [ST7735_PWCTR2, 0xC5],
+    # PWCTR3 - Set amplifier current & booster cycles (Full Colors Normal Mode)
+    [ST7735_PWCTR3, 0x0A, 0x00],
+    # PWCTR4 - Set amplifier current & booster cycles (Idle Mode)
+    [ST7735_PWCTR4, 0x8A, 0x2A],
+    # PWCTR5 - Set amplifier current & booster cycles (Full Colors Partial Mode)
+    [ST7735_PWCTR5, 0x8A, 0xEE],
+    # VMCTR1 - Set VCOMH & VCOML voltage
+    [ST7735_VMCTR1, 0x0E],
+    # INVOFF - Leave display inversion mode
+    [ST7735_INVOFF],
+    # COLMOD - Interface Pixel Format
+    # 0x03 - 12-bit/pixel; 0x05 - 16-bit/pixel; 0x05 - 18-bit/pixel;
+    [ST7735_COLMOD, 0x05],
+    [ST7735_MADCTL, 0x08],
+    # CASET - Set column range
+    [ST7735_CASET, 0x00,0x00,0x00,0x4F],
+    # RASET - Set row range
+    [ST7735_RASET, 0x00,0x00,0x00,0x9F],
+    # Gamma commands
+    # Param 1 - High level adjustment; Param 2-15 - Mid level adjustment; Param 16 - Low level adjustment
+    # GMCTRP1 - Gamma ('+' polarity)
+    [ST7735_GMCTRP1, 0x02, 0x1C, 0X07, 0X12, 0X37, 0X32, 0x29, 0x2D, 0X29, 0X25, 0X2B, 0X39, 0X00, 0X01, 0X03, 0x10],
+    # GMCTRN1 - Gamma ('-' polarity)
+    [ST7735_GMCTRN1, 0x03, 0x1D, 0X07, 0X06, 0X2E, 0X2C, 0x29, 0x2D, 0X2E, 0X2E, 0X37, 0X3F, 0X00, 0X00, 0X02, 0x10],
+    # NORON - Normal display mode on
+    [ST7735_NORON],
+    # DISPON - Display on
+    [ST7735_DISPON]
+]
+
+bitmask = const((128, 64, 32, 16, 8, 4, 2, 1))
+bitmask_inv = const((127, 191, 223, 239, 247, 251, 253, 254))
+
+def int16_to_bytes(i: int):
+    return bytes([(i >> 8) & 0xFF, i & 0xFF])
+
+def rgb_to_565(rgb):
+    r, g, b = rgb
+    # Convert RGB values to 5-6-5 format
+    r5 = (r >> 3) & 0x1F  # 5 bits for red
+    g6 = (g >> 2) & 0x3F  # 6 bits for green
+    b5 = (b >> 3) & 0x1F  # 5 bits for blue
+    return (r5 << 11) | (g6 << 5) | b5
+
+# A mono-only frame buffer built for fast pixel yields
+class MonoFrameBuffer(framebuf.FrameBuffer):
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.draw_buf_size = ceil((width * height) / 8)
+        self.draw_buf = array("B", bytes(self.draw_buf_size * [0x00]))
+        self.draw_buf_ref = memoryview(self.draw_buf)
+        super().__init__(self.draw_buf_ref, width, height, framebuf.MONO_HLSB)
+
+    def yield_px_in_row(self, y, start_x, end_x):
+        buf = self.draw_buf_ref
+        buf_width = self.width
+        bottom_pos = y * buf_width + start_x
+        top_pos = bottom_pos + end_x
+        start_byte = floor(bottom_pos / 8)
+        pos = start_byte * 8
+
+        for b in buf[start_byte:ceil(top_pos / 8) + 1]:
+            while b > 0:
+                bitlen = 7 - int(log(b,2))
+                b = b & bitmask_inv[bitlen]
+                new_pos = pos + bitlen
+                if new_pos < bottom_pos:
+                    continue
+                elif new_pos > top_pos:
+                    return
+                # Yield the x
+                yield new_pos % buf_width
+            pos += 8
+
+    def yield_lines_in_row(self, y, start_x=0, end_x=None):
+        end_x = self.width if None else end_x
+        next_x = start_x
+        line_width = 0
+        for px in self.yield_px_in_row(y, start_x, end_x):
+            if px == next_x:
+                line_width += 1
+                next_x += 1
+                continue
+            elif line_width > 0:
+                yield next_x - line_width, next_x - 1
+            next_x = px + 1
+            line_width = 1
+        if line_width > 0:
+            yield next_x - line_width, next_x - 1
+
+    def set_mono_framebuf_pixel(self, x, y, p):
+        buf = self.draw_buf_ref
+        pos = (y * self.width) + x
+        mod = pos % 8
+        pos = int((pos - mod) / 8)
+        if p == 0:
+            buf[pos] = buf[pos] & bitmask_inv[mod]
+        else:
+            buf[pos] = buf[pos] | bitmask[mod]
+        
+class ST7735:
+    def __init__(self, dc=22, cs=21, rt=20, sck=18, mosi=19, miso=16, spi_port=0, baud=62_500_000, height=160, width=80, cache_font=True):
+        self.dc_pin = Pin(dc, Pin.OUT, value=1)
+        self.cs_pin = Pin(cs, Pin.OUT, value=1)
+        self.rt_pin = Pin(rt, Pin.OUT, value=1)
+
+        self.sck_pin = Pin(sck, Pin.ALT_SPI)
+        self.mosi_pin = Pin(mosi, Pin.ALT_SPI)
+        self.miso_pin = Pin(miso, Pin.ALT_SPI)
+
+        self.height = height
+        self.width = width
+        self.c_offset = 24
+        self.r_offset = 0
+        self.flipped = False
+
+        self.mono_fb = MonoFrameBuffer(self.width, self.height)
+
+        # Theorhetical max is half of the system frequency (125MHz / 2)
+        self.spi = SPI(spi_port, baud, polarity=0, phase=0, firstbit=SPI.MSB, sck=self.sck_pin, mosi=self.mosi_pin, miso=self.miso_pin)
+
+        self.font_cache : array
+        self.font_cache_lookup : array
+        if cache_font:
+            self.font_cache_lookup = array("h", 127 * [0])
+            self.build_font_cache()
+        self._draw_to_cache = None
+
+    def send_command(self, cmd, args = None):
+        self.cs_pin.low()
+        self.dc_pin.low()
+        self.spi.write(bytes([cmd]))
+        self.cs_pin.high()
+        self.dc_pin.high()
+
+        if args is not None and len(args) > 0:
+            self.cs_pin.low()
+            self.spi.write(bytes(args))
+            self.cs_pin.high()
+            self.dc_pin.high()
+
+    def tft_initialize(self):
+        self.rt_pin.low()
+        time.sleep_ms(100)
+        self.rt_pin.high()
+        time.sleep_ms(220)
+
+        for cmd in init_cmds:
+            self.send_command(cmd[0], None if len(cmd) == 1 else cmd[1:])
+
+    def set_rotation(self, rotation, mirror_x=False, mirror_y=False):
+        r = rotation % 4
+
+        flipped = rotation % 2 == 1
+        self.c_offset = 24 if not flipped else 0
+        self.r_offset = 24 if flipped else 0
+        if flipped != self.flipped:
+            h = self.height
+            self.height = self.width
+            self.width = h
+            self.flipped = flipped
+
+        madctl_arg = (0x08, 0x6C, 0xDC, 0xB8)[r]
+        if mirror_x:
+            madctl_arg = madctl_arg ^ 0x40
+        if mirror_y:
+            madctl_arg = madctl_arg ^ 0x80
+        self.send_command(ST7735_MADCTL, [madctl_arg])
+
+    def draw_rect(self, x, y, w, h, c: int | bytes, fill=True, thickness=1):
+        if isinstance(c, int):
+            c = int16_to_bytes(c)
+
+        if fill:
+            if self._draw_to_cache is not None:
+                self._draw_to_cache.add_rect(x, y, w, h, c)
+            else:
+                # Set column range
+                c_offset = self.c_offset
+                caset_args = b''.join((
+                    int16_to_bytes(c_offset + x),
+                    int16_to_bytes(c_offset + x + w - 1)
+                ))
+                self.send_command(ST7735_CASET, caset_args)
+                # Set row range
+                r_offset = self.r_offset
+                raset_args = b''.join((
+                    int16_to_bytes(r_offset + y),
+                    int16_to_bytes(r_offset + y + h - 1)
+                ))
+                self.send_command(ST7735_RASET, raset_args)
+                # Start memory write
+                self.send_command(ST7735_RAMWR)
+
+                self.cs_pin.low()
+                self.spi.write(c * (w * h))
+                self.cs_pin.high()
+                self.dc_pin.high()
+        else:
+            half_thick = int(thickness / 2)
+            self.draw_rect(x - half_thick, y - half_thick, w + thickness, thickness, c)
+            self.draw_rect(x - half_thick, y + h - half_thick, w + thickness, thickness, c)
+            self.draw_rect(x - half_thick, y + half_thick, thickness, h - thickness, c)
+            self.draw_rect(x + w - half_thick, y + half_thick, thickness, h - thickness, c)
+
+    # Compose the pixels in the framebuffer into rectangles. Used for faster drawing.
+    # Return format is a list of bytes in the format [rect1_x, rect1_y, rect1_w, rect1_h, rect2_x...]
+    def find_rects_in_fb(self, start_x, end_x, start_y, end_y):
+        # Helper function for checking if a line of pixels can extend down one level
+        def can_expand_down(start_x, end_x, y):
+            for line_start_x,line_end_x in self.mono_fb.yield_lines_in_row(y, start_x, end_x):
+                return line_start_x == start_x and line_end_x == end_x
+
+        def get_expanded_rect(start_x, end_x, y):
+            h = 1
+            next_row = y + 1
+            while can_expand_down(start_x, end_x, next_row):
+                # If it did expand down, unset the pixels expanded
+                for exp_x in range(start_x, end_x + 1):
+                    self.mono_fb.set_mono_framebuf_pixel(exp_x, next_row, 0)
+                next_row += 1
+                h += 1
+            return (start_x, y, end_x - start_x + 1, h)
+    
+        rects = []
+        # For each row and column
+        for y in range(start_y,end_y+1):
+            for line_start_x,line_end_x in self.mono_fb.yield_lines_in_row(y, start_x, end_x):
+                rects += get_expanded_rect(line_start_x, line_end_x, y)
+
+        return rects
+        
+    # Draw each ASCII characters 33-126, decompose the characters into rectangles, and cache them for faster drawing
+    def build_font_cache(self):
+        font_cache_pos = 0
+        all_rects = []
+
+        for c in range(127):
+            if c < 33:
+                self.font_cache_lookup[c] = -1
+                continue
+            # Get the frame buffer to draw the character
+            self.mono_fb.fill_rect(0, 0, 8, 8, 0)
+            self.mono_fb.text(chr(c), 0, 0, 1)
+
+            char_rects = self.find_rects_in_fb(0, 7, 0, 7)
+
+            self.font_cache_lookup[c] = font_cache_pos
+            len_char_rects = len(char_rects)
+            font_cache_pos += len_char_rects + 1
+            all_rects += [int(len(char_rects) / 4)] + char_rects
+
+        self.font_cache = array("B", all_rects)
+
+    # Draw the pixels in the region defined in the frame buffer
+    def draw_fb_pixels(self, start_x, end_x, start_y, end_y, c, convex=False):
+        # Gain a few ms by making local function references
+        draw_rect = self.draw_rect
+
+        for y in range(start_y, end_y + 1):
+            for line_start_x,line_end_x in self.mono_fb.yield_lines_in_row(y, start_x, end_x):
+                draw_width = line_end_x - line_start_x + 1
+                draw_rect(line_start_x, y, draw_width, 1, c)
+                if convex:
+                    break
+
+    def fill_screen(self, c):
+        self.draw_rect(0, 0, self.width, self.height, c)
+
+    # Draw text pixel by pixel
+    def draw_text(self, text, x, y, c):
+        text_width = min(8 * len(text), self.width - x)
+        text_height = min(8, self.height - y)
+
+        self.mono_fb.fill_rect(x, y, text_width, text_height, 0)
+        self.mono_fb.text(text, x, y, 1)
+        self.draw_fb_pixels(x, x + text_width, y, y + text_height, c)
+
+    # Draw text using the font cache
+    def draw_fast_text(self, text: str, x, y, c, bgc=None):
+        x_pos = x
+        cache_len = len(self.font_cache_lookup)
+
+        for symbol in text:
+            if bgc is not None:
+                self.draw_rect(x_pos, y, 8, 8, bgc)
+
+            symbol_ord = ord(symbol)
+            if symbol_ord < cache_len:
+                # Use the lookup to find where the data for this character is in the font cache
+                font_cache_pos = self.font_cache_lookup[symbol_ord]
+                if font_cache_pos > -1:
+                    # The first byte tells you how many rectangles are in this character
+                    num_rects = self.font_cache[font_cache_pos] 
+                    for _ in range(num_rects):
+                        self.draw_rect(
+                            self.font_cache[font_cache_pos + 1] + x_pos, 
+                            self.font_cache[font_cache_pos + 2] + y, 
+                            self.font_cache[font_cache_pos + 3], 
+                            self.font_cache[font_cache_pos + 4],
+                            c)
+                        # Advance 4 bytes to the next rectangle
+                        font_cache_pos += 4
+            x_pos += 8
+            if x_pos > self.width:
+                return
+
+    def draw_hline(self, x, y, w, c):
+        self.draw_rect(x, y, w, 1, c)
+
+    def draw_vline(self, x, y, h, c):
+        self.draw_rect(x, y, 1, h, c)
+
+    def draw_line(self, x1, y1, x2, y2, c):
+        min_x = min(x1, x2)
+        max_x = max(x1, x2)
+        min_y = min(y1, y2)
+        max_y = max(y1, y2)
+        self.mono_fb.fill_rect(min_x, min_y, max_x - min_x, max_y - min_y, 0)
+        self.mono_fb.line(x1, y1, x2, y2, 1)
+        self.draw_fb_pixels(min_x, max_x, min_y, max_y, c, convex=True)
+
+    def draw_poly(self, x, y, coords, c, fill=True, convex=False):
+        coord_array = array("i", coords)
+        coord_len = len(coord_array)
+        x_max = max([coord_array[i] for i in range(0, coord_len, 2)])
+        y_max = max([coord_array[i] for i in range(1, coord_len, 2)])
+        self.mono_fb.fill_rect(x, y, x_max - x + 1, y_max - y + 1, 0)
+        self.mono_fb.poly(x, y, coord_array, c, fill)
+        self.draw_fb_pixels(x, x_max, y, y_max, c, convex)
+
+    def draw_ellipse(self, x, y, rx, ry, c, fill=True):
+        # Fill the top-left quadrant and use that to draw the whole shape
+        self.mono_fb.fill_rect(x - rx, y - ry, rx, ry, 0)
+        self.mono_fb.ellipse(x, y, rx, ry, 1, False, 2)
+
+        # For each row and column
+        rect_height = ry * 2 if fill else 1
+        for buf_y in range(y-ry,y):
+            try:
+                rect_start_x,rect_end_x = next(self.mono_fb.yield_lines_in_row(buf_y, x-rx, x))
+            except StopIteration:
+                continue
+            rect_width = rect_end_x - rect_start_x + 1
+            # Draw the left rectangle
+            self.draw_rect(rect_start_x, buf_y, rect_width, rect_height, c)
+            # Draw the right rectangle
+            self.draw_rect(x + (x - (rect_start_x + rect_width)), buf_y, rect_width, rect_height, c)
+
+            if fill is False:
+                # Draw the left bottom rectangle
+                self.draw_rect(rect_start_x, y + (y - buf_y), rect_width, rect_height, c)
+                # Draw the right bottom rectangle
+                self.draw_rect(x + (x - (rect_start_x + rect_width)), y + (y - buf_y), rect_width, rect_height, c)
+            else:
+                rect_height -= 2
+        
+        
--- pico/main.py
+++ pico/main.py
@@ -1,22 +1,141 @@
-from ADS1115 import ADS1115
+from ads1x15 import ADS1015
+from ST7735 import ST7735
 from machine import I2C, Pin
 import sys
+import time
 
-SDA_Pin = Pin(14)
-SCL_Pin = Pin(15)
-port = I2C(id=1, scl=SCL_Pin, sda=SDA_Pin, freq=400000)
-print(port.scan())
+CONV_2V_5V_DIFF = 5 / 1.798
+CONV_2V_5V_COMMON = 5 / 1.799
+CONV_2V_12V_COMMON = 12 / 1.797
+CONV_2V_20V_COMMON = 20 / 1.818
+BACK_COLOUR = 0x0000
+FRONT_1_COLOUR = 0xDEDB
+FRONT_2_COLOUR = 0xA514
+HIGHLIGHT_1_COLOUR = 0x296B
+HIGHLIGHT_2_COLOUR = 0x7380
+CHANNEL_TXT_Y = 15
+CHANNEL_TXT_GAP = 11
+POWER_TXT_X = 96
+POWER_TXT_Y = 14
+POWER_TXT_BUFFER = 3
+POWER_TXT_GAP = 11
 
-ADS = ADS1115(i2c=port, gain=0)
+def draw_background(tft: ST7735):
+    tft.fill_screen(BACK_COLOUR)
+    tft.draw_rect(0, 0, 180, 10, HIGHLIGHT_1_COLOUR)
+    tft.draw_fast_text("POWER MONITOR", 32, 2, FRONT_1_COLOUR)
+    tft.draw_fast_text("Ch0:      V", 5, CHANNEL_TXT_Y, FRONT_2_COLOUR)
+    tft.draw_fast_text("Ch1:      V", 5, CHANNEL_TXT_Y + CHANNEL_TXT_GAP, FRONT_2_COLOUR)
+    tft.draw_fast_text("Ch2:      V", 5, CHANNEL_TXT_Y + CHANNEL_TXT_GAP * 2, FRONT_2_COLOUR)
+    tft.draw_fast_text("Ch3:      V", 5, CHANNEL_TXT_Y + CHANNEL_TXT_GAP * 3, FRONT_2_COLOUR)
+
+    tft.draw_rect(POWER_TXT_X, POWER_TXT_Y, 61, 46, HIGHLIGHT_2_COLOUR)
+    tft.draw_fast_text("     V", POWER_TXT_X + 3, POWER_TXT_Y + POWER_TXT_BUFFER, FRONT_1_COLOUR)
+    tft.draw_fast_text("     A", POWER_TXT_X + 3, POWER_TXT_Y + POWER_TXT_BUFFER + POWER_TXT_GAP, FRONT_1_COLOUR)
+    tft.draw_fast_text("     W", POWER_TXT_X + 3, POWER_TXT_Y + POWER_TXT_BUFFER + POWER_TXT_GAP * 2, FRONT_1_COLOUR)
+    tft.draw_fast_text("     Wh", POWER_TXT_X + 3, POWER_TXT_Y + POWER_TXT_BUFFER + POWER_TXT_GAP * 3, FRONT_1_COLOUR)
+
+    tft.draw_hline(3, 59, 90, HIGHLIGHT_1_COLOUR)
+
+def draw_channel_volts(tft: ST7735, channel, v):
+    y = CHANNEL_TXT_Y + (CHANNEL_TXT_GAP * channel)
+    tft.draw_fast_text(f"{round(v, 3):.3f}", 42, y, FRONT_2_COLOUR, BACK_COLOUR)
+
+def draw_volts(tft: ST7735, v):
+    tft.draw_fast_text(f"{round(v, 3):05.3f}", POWER_TXT_X + 2, POWER_TXT_Y + POWER_TXT_BUFFER, FRONT_1_COLOUR, HIGHLIGHT_2_COLOUR)
+
+def draw_amps(tft: ST7735, a):
+    tft.draw_fast_text(f"{round(a, 2):05.2f}", POWER_TXT_X + 2, POWER_TXT_Y + POWER_TXT_BUFFER + POWER_TXT_GAP, FRONT_1_COLOUR, HIGHLIGHT_2_COLOUR)
+
+def draw_watts(tft: ST7735, w):
+    tft.draw_fast_text(f"{round(w, 1):05.1f}", POWER_TXT_X + 2, POWER_TXT_Y + POWER_TXT_BUFFER + POWER_TXT_GAP * 2, FRONT_1_COLOUR, HIGHLIGHT_2_COLOUR)
+
+def draw_watt_hours(tft: ST7735, wh):
+    tft.draw_fast_text(f"{int(wh):05d}", POWER_TXT_X + 2, POWER_TXT_Y + POWER_TXT_BUFFER + POWER_TXT_GAP * 3, FRONT_1_COLOUR, HIGHLIGHT_2_COLOUR)
+
+def draw_time(tft: ST7735, secs):
+    h = int(secs / 3600)
+    m = int((secs % 3600) / 60)
+    s = secs % 60
+    tft.draw_fast_text(f"{h:02d}h {m:02d}m {s:02d}s", 40, 65, FRONT_2_COLOUR, BACK_COLOUR)
+
+
+# Setup ADS1015
+SDA_Pin = Pin(16)
+SCL_Pin = Pin(17)
+port = I2C(id=0, scl=SCL_Pin, sda=SDA_Pin, freq=400000)
+ADS = ADS1015(port, 72, 2)
+
+# Setup TFT
+tft = ST7735(dc=28, cs=21, rt=27, sck=18, mosi=19, miso=20)
+tft.tft_initialize()
+tft.set_rotation(1)
+draw_background(tft)
+
+prev_ch_v = [0, 0, 0, 0]
+ch_v = [0, 0, 0, 0]
+
+prev_in_v = -1
+in_v = 0
+
+prev_amps = -1
+amps = 0
+
+prev_secs = -1
+secs = 0
+
+prev_wh = -1
+wh = 0
+
+prev_ticks = None
+ticks = 0
+
+start_time = time.time()
 
 while True:
-    acs_1_diff_v = ADS.raw_to_v(ADS.read(rate=0, channel1=0, channel2=1))
-    in_v_1 = ADS.raw_to_v(ADS.read(rate=0, channel1=2))
-    lm35_1_v = ADS.raw_to_v(ADS.read(rate=0, channel1=3))
+    ch_v = [
+        ADS.raw_to_v(ADS.read(0, 0)),
+        ADS.raw_to_v(ADS.read(0, 1)),
+        ADS.raw_to_v(ADS.read(0, 2)),
+        ADS.raw_to_v(ADS.read(0, 3))
+    ]
 
-    amps_1 = (acs_1_diff_v * 1000) / 66
-    temp_1 = lm35_1_v * 100
+    for i in range(4):
+        if ch_v[i] != prev_ch_v[i]:
+            draw_channel_volts(tft, i, ch_v[i])
+    prev_ch_v = ch_v
 
-    sys.stdout.write(f"dc_1_voltage,{in_v_1}\n")
-    sys.stdout.write(f"dc_1_current,{amps_1}\n")
-    sys.stdout.write(f"temp_1,{temp_1}\n")
+    in_v = ch_v[2] * CONV_2V_5V_COMMON
+    if in_v != prev_in_v:
+        draw_volts(tft, in_v)
+
+    acs_diff_v = ADS.raw_to_v(ADS.read(0, 0, 3)) * CONV_2V_5V_DIFF
+    amps = (acs_diff_v * 1000) / 66
+
+    ticks = time.ticks_ms()
+    if prev_ticks is not None:
+        ms_delta = time.ticks_diff(ticks, prev_ticks)
+        h_delta = (ms_delta / 1000 / 3600)
+        wh += in_v * amps * h_delta
+    prev_ticks = time.ticks_ms()
+
+    if amps != prev_amps:
+        draw_amps(tft, amps)
+    
+    if amps != prev_amps or in_v != prev_in_v:
+        draw_watts(tft, in_v * amps)
+
+    if int(wh) != prev_wh:
+        draw_watt_hours(tft, wh)
+    prev_wh = int(wh)
+
+    prev_in_v = in_v
+    prev_amps = amps
+
+    secs = time.time() - start_time
+    if secs != prev_secs:
+        draw_time(tft, secs)
+    prev_secs = secs
+
+    sys.stdout.write(f"dc_voltage,{in_v}\n")
+    sys.stdout.write(f"dc_current,{amps}\n")
